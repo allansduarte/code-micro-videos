32f630e5c99b86981f62b67b9de2daf7c43848e4
Allan <allan.sduarte@gmail.com>
2021-06-24 12:10:28 -0300

    chore: docker-compose settings

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,7 +2,9 @@
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 chown -R www-data:www-data .
+
 composer install
+
 php artisan key:generate
 php artisan migrate
 
